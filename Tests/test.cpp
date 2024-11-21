4dab7d846a58746bdc3d69473b2f544db79b0390
loveyoupeng <loveyoupeng@gmail.com>
2024-11-22 01:46:17 +0800

    atmoic test case

--- Tests/test.cpp
+++ Tests/test.cpp
@@ -1,8 +1,18 @@
 #include <gtest/gtest.h>
 #include <juce_dsp/juce_dsp.h>
+#include <atomic>
 
 namespace test_plugins
 {
+    struct A {int a[2];};
+    struct B {int a[5];};
+    TEST(Cpp, lock_free)
+    {
+        std::atomic<A> a;
+        std::atomic<B> b;
+        EXPECT_TRUE(a.is_lock_free());
+        EXPECT_FALSE(b.is_lock_free());
+    }
 
     TEST(Juce, Oscillator)
     {
