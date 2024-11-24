079fb8b0fde2dcd3ff51800a8f22b0d6d356221d
loveyoupeng <loveyoupeng@gmail.com>
2024-11-24 23:35:24 +0800

    test cases for osc and adsr

--- Tests/SimpleTests.cpp
+++ Tests/SimpleTests.cpp
@@ -1,11 +1,19 @@
 #include <gtest/gtest.h>
 #include <juce_dsp/juce_dsp.h>
 #include <atomic>
+#include <cmath>
+#include <juce_audio_basics/juce_audio_basics.h>
 
 namespace test_plugins
 {
-    struct A {int a[2];};
-    struct B {int a[5];};
+    struct A
+    {
+        int a[2];
+    };
+    struct B
+    {
+        int a[5];
+    };
     TEST(Cpp, lock_free)
     {
         std::atomic<A> a;
@@ -19,10 +27,59 @@ namespace test_plugins
         juce::dsp::Oscillator<float> osc;
         EXPECT_FALSE(osc.isInitialised());
         osc.initialise([](float x)
-                       { return x; });
+                       { return std::sin(x); });
         EXPECT_TRUE(osc.isInitialised());
         osc.setFrequency(1);
         EXPECT_EQ(1.0, osc.getFrequency());
+        osc.reset();
+        for (int i = 0; i < 48000 / 4; i++)
+            osc.processSample(.0f);
+        EXPECT_NEAR(-1.0f, osc.processSample(.0f), 0.0001);
+
+        for (int i = 0; i < 48000 / 4; i++)
+            osc.processSample(.0f);
+        EXPECT_NEAR(0, osc.processSample(.0f), 0.0001);
+
+        for (int i = 0; i < 48000 / 4; i++)
+            osc.processSample(.0f);
+        EXPECT_NEAR(1.f, osc.processSample(.0f), 0.0001);
+
+        for (int i = 0; i < 48000 / 4; i++)
+            osc.processSample(.0f);
+
+        EXPECT_NEAR(0.f, osc.processSample(.0f), 0.01);
+        for (int i = 0; i < 48000 / 4; i++)
+            osc.processSample(.0f);
+
+        EXPECT_NEAR(-1.f, osc.processSample(.0f), 0.01);
+    }
+
+    TEST(Cpp, sin)
+    {
+        EXPECT_DOUBLE_EQ(0.0, std::sin(0));
+        EXPECT_DOUBLE_EQ(1.0, std::sin(0.5 * std::numbers::pi));
+    }
+
+    TEST(Juce, ADSR)
+    {
+        juce::ADSR adsr;
+        juce::ADSR::Parameters parameters{1, 1, 0.5, 1};
+        adsr.setSampleRate(10.0);
+        adsr.setParameters(parameters);
+        EXPECT_FLOAT_EQ(0.0, adsr.getNextSample());
+        adsr.noteOn();
+        for (int i = 0; i < 10; i++)
+            EXPECT_FLOAT_EQ(0.1 * (i + 1), adsr.getNextSample());
+
+        for (int i = 0; i < 10; i++)
+            EXPECT_FLOAT_EQ(1.0 - 0.05 * (i + 1), adsr.getNextSample());
+
+        for (int i = 0; i < 100; i++)
+            EXPECT_FLOAT_EQ(0.5, adsr.getNextSample());
+
+        adsr.noteOff();
+        for (int i = 0; i < 10; i++)
+            EXPECT_NEAR(0.5 - 0.05 * (i + 1), adsr.getNextSample(), 0.0001);
     }
 
 } // namespace test_plugins
